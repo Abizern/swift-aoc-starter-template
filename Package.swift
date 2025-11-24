ec3722738bfbcad1443a3a8cde0138bb58d65c31
Abizer Nasir <abizern@mac.com>
2025-11-24 10:57:39 +0000

    Update Package dependency versions

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 6.2
 import PackageDescription
 
 let dependencies: [Target.Dependency] = [
@@ -13,20 +13,16 @@ let package = Package(
   platforms: [.macOS(.v15)],
   dependencies: [
     .package(
-      url: "https://github.com/apple/swift-algorithms.git",
-      from: "1.2.0"
+      url: "https://github.com/apple/swift-algorithms.git", "1.2.1"..<"1.3.0"
     ),
     .package(
-      url: "https://github.com/apple/swift-argument-parser.git",
-      from: "1.5.0"
+      url: "https://github.com/apple/swift-argument-parser.git", "1.6.2"..<"1.7.0"
     ),
     .package(
-      url: "https://github.com/apple/swift-collections.git",
-      from: "1.1.0"
+      url: "https://github.com/apple/swift-collections.git", "1.3.0"..<"1.4.0"
     ),
     .package(
-      url: "https://github.com/pointfreeco/swift-parsing",
-      from: "0.13.0"
+      url: "https://github.com/pointfreeco/swift-parsing", "0.14.1"..<"0.15.0"
     ),
     .package(path: "NewDayPlugin"),
   ],
